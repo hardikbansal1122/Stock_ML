f6ded3f526e74788eed4b23b4702b5f961105d5d
hardik <hardikbansal1122@gmail.com>
2026-07-24 23:35:02 +0530

    Increase download workers from 8 to 12

--- stock_dashboard/app.py
+++ stock_dashboard/app.py
@@ -43,7 +43,7 @@ ROOT_DIR       = BASE_DIR.parent
 MODEL_PATH     = BASE_DIR / 'xgb_model.pkl'
 SCALER_PATH    = BASE_DIR / 'scaler.pkl'
 FEAT_PATH      = BASE_DIR / 'feature_list.csv'
-MAX_DOWNLOAD_WORKERS = 8
+MAX_DOWNLOAD_WORKERS = 12
 
 sys.path.insert(0, str(ROOT_DIR))
 from universe import get_universe_tickers, normalize_ticker
